7833e64f1a0c0738f450c84cd65c77c7514934f7
Michelle Jowers <michelle.jowers@icloud.com>
2021-05-28 15:34:43 -0400

    update queries

--- food-db/food_db_joins-1.sql
+++ food-db/food_db_joins-1.sql
@@ -5,7 +5,9 @@ select orderticketID, name, price, quantity, (price * quantity) as subtotal
      on MenuItemID = mi.id
 where OrderTicketID = 2;
 
-select orderticketid, lastname, firstname, cat.name as category, mi.name as item, price, quantity, (price * quantity) as subtotal
+select OrderTicketID as 'Order ID', concat(lastname, ', ', firstname) as 'Customer Name', cat.name as Category,
+  mi.name as Item, concat('$', ' ', price) as Price, Quantity as Qty, 
+  concat('$', ' ', (price * quantity)) as Subtotal
 	from lineitem
     join menuitem mi
      on MenuItemID = mi.id
@@ -16,3 +18,10 @@ select orderticketid, lastname, firstname, cat.name as category, mi.name as item
 	join category cat
      on categoryid = cat.id
 where OrderTicketID = 3;
+
+-- calculate total for an order
+select sum(price * Quantity) as total
+ from lineitem
+ join menuitem mi on menuitemid = mi.id
+where orderticketid = 2
+group by OrderTicketID;
---
+++ food-db/food_db_update_query-3.sql
@@ -0,0 +1,7 @@
+select * from orderticket;
+update orderticket set
+	status = 'C', Total = 43.96
+    where id = 2;
+    
+    
+    
