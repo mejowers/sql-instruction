2616c6d877859fe6ed87629ed36b34c6bbd061a7
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-28 19:35:15 -0400

    update files

---
+++ MySQL/PRS/prs_inserts_lineitems.sql
@@ -0,0 +1,11 @@
+SELECT * FROM prs.lineitem;
+insert into lineitem (RequestID, ProductID, Quantity)Values
+(1, 4, 1),
+(1, 11, 1),
+(2, 12, 1),
+(2, 14, 1),
+(2, 6, 1),
+(3, 9, 1),
+(3, 15, 1),
+(3, 6, 1);
+
