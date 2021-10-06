cb6c746c4b82d7037f99792223b408af41efcc07
Michelle Jowers <michelle.jowers@icloud.com>
2021-10-05 21:22:55 -0400

    sql inserts

---
+++ SQL Server/BmDb/Inserts-BmDb2.sql
@@ -0,0 +1,57 @@
+use BmDb2;
+INSERT into Movies (Title, Year, Rating, Director) values 
+ 
+ 	('Revenge of the Nerds', 1984, 'R', 'Jeff Kanew'),
+ 	('Avengers Infinity War', 2018, 'PG-13', 'Anthony Russo, John Russo'),
+    ('Thor', 2011, 'PG-13', 'Kenneth Branagh'),
+    ('Twilight', 2008, 'PG-13', 'Catherine Hardwick'),
+    ('Rocky', 1976, 'PG', 'John G. Avildsen'),
+    ('Silence of the Lambs', 1991, 'R', 'Jonathon Demme'),
+    ('The Shawshank Redemption', 1994, 'R', 'Frank Darabont'),
+    ('Braveheart', 1995, 'R', 'Mel Gibson'),
+    ('Dances with Wolves', 1990, 'PG-13', 'Kevin Costner'),
+    ('Gran Torino', 2008, 'R', 'Clint Eastwood');
+
+	 insert into Actors(FirstName, LastName, Gender, BirthDate)  VALUES
+ 	('Robert', 'Downey Jr.', 'M', '1965-4-4'),
+ 	('Chris', 'Hemsworth', 'M', '1983-08-11'),
+    ('Scarlett', 'Johansson', 'F', '1984-11-22'),
+ 	('Robert', 'Carradine', 'M', '1954-3-24'),
+ 	('Anthony', 'Edwards', 'M', '1962-7-19'),
+    ('Anthony', 'Hopkins', 'M', '1937-12-31'),
+    ('Robert', 'Pattinson', 'M', '1986-05-13'),
+    ('Kristen', 'Stewart', 'F', '1990-04-09'),
+    ('Sylvester', 'Stalone', 'M', '1946-07-06'),
+    ('Talia', 'Shire', 'F', '1946-04-25'),
+    ('Jodie', 'Foster', 'F', '1962-11-19'),
+    ('Tim', 'Robbins', 'M', '1958-10-16'),
+    ('Morgan', 'Freeman', 'M', '1937-06-01'),
+    ('Mel', 'Gibson', 'M', '1956-01-03'),
+    ('Catherine', 'McCormick', 'F', '1972-04-03'),
+    ('Kevin', 'Costner', 'M', '1955-01-18'),
+    ('Mary', 'McDonnel', 'F', '1952-04-28'),
+    ('Clint', 'Eastwood', 'M', '1930-05-31'),
+    ('Ahney', 'Her', 'F', '1992-07-13');
+
+	 insert into Credits(ActorID, MovieID, Role) VALUES
+ 	(1, 2, 'Tony Stark - Iron Man'),
+    (2, 2, 'Thor'),
+    (3, 2, 'Natasha Romanoff - Black Widow'),
+    (4, 1, 'Lewis'),
+    (5, 1, 'Gilbert'),
+    (2, 3, 'Thor'),
+    (6, 3, 'Odin'),
+    (7, 4, 'Edward Cullen'),
+    (8, 4, 'Bella Swan'),
+    (9, 5, 'Rocky'),
+    (10, 5, 'Adrian'),
+    (6, 6, 'Dr. Hannibal Lecter'),
+    (11, 6, 'Clarice Starling'),
+    (12, 7, 'Andy Dufresne'),
+    (13, 7, 'Ellis Boyd - Red - Redding'),
+    (14, 8, 'William Wallace'),
+    (15, 8, 'Murron'), 
+    (16, 9, 'Lieutenant Dunbar'),
+    (17, 9, 'Stands with a Fist'),
+    (18, 10, 'Walt Kowalski'),
+    (19, 10, 'Sue');
---
+++ SQL Server/BmDb/MovieCollections-inserts-bmdb2.sql
@@ -0,0 +1,9 @@
+INSERT into MovieCollections (MovieId, PurchasePrice, UserId) VALUES
+	(3, 25.99, 2),
+	(4, 29.99, 2),
+	(1, 19.99, 3),
+	(5, 22.99, 3),
+	(10, 19.98, 3),
+	(7, 25.99, 5),
+	(2, 27.98, 5),
+	(6, 19.99, 5);
