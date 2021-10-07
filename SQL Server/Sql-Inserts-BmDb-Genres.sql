5e51a6e8cbe4aa14daf3bac1a94b19fce06eda88
Michelle Jowers <michelle.jowers@icloud.com>
2021-10-06 20:01:10 -0400

    add inserts to genre

---
+++ SQL Server/Sql-Inserts-BmDb-Genres.sql
@@ -0,0 +1,5 @@
+INSERT into Genres(Name) values 
+	('Action'), ('Adventure'), ('Animation'), ('Biography'),
+	('Comedy'), ('Crime'), ('Drama'), ('Family'), ('Fantasy'),
+	('History'), ('Horror'), ('Musical'), ('Mystery'), ('Romance'), 
+	('Sci-Fi'), ('Sport'), ('Thriller'), ('War'), ('Western');
